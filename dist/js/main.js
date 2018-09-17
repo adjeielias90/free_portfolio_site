13bbfde7605020ac7189731f21a059498b71e45b
adjeielias90 <adjeielias@gmail.com>
2018-09-17 17:16:16 +0000

    js, images and animations

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+//select classes and ids from the dom
+
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+//set intial state of menu: Reset
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+    //reset menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+    //set menu state
+    showMenu = false;
+  }
+}
